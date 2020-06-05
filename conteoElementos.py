afc41177f6bdb05b2ff3964fcb44a6c2eb34f3e8
virulilla <e.salido.castro@gmail.com>
2020-06-05 19:13:39 +0200

    Se añade funcionalidad

--- conteoElementos.py
+++ conteoElementos.py
@@ -1,32 +1,53 @@
 import arcpy, os
 from datetime import datetime, timedelta
 
-mdb = ""
-flag = False
-while not flag:
-    path = input("Ubicacion de " + mdb + ": ")
-    arcpy.env.workspace = path
-    if arcpy.Exists(mdb):
-        flag = True
-    else:
-        print("Este directorio no contiene el mdb")
 
-f = open(os.path.join(path, "conteo.txt"), 'w')
-replica = os.path.join(path, "replica.gdb")
-if arcpy.Exists(replica):
-    arcpy.Delete_management(replica)
-arcpy.CreateFileGDB_management(path, "replica")
-arcpy.TableToTable_conversion(os.path.join(mdb, ""), replica, "")
-table = arcpy.env.workspace = os.path.join(path, replica, "")
+print "Inicio " + str(datetime.now().time())
+fRecibido = open("C://CYII//conteoElementos//data//recibido//RA_58270//recibido.txt", 'w')
+fEnviado = open("C://CYII//conteoElementos//data//enviado//RA_58270//enviado.txt", 'w')
+mdbRecibido = "C://CYII//conteoElementos//data//recibido//RA_58270//replica.mdb"
+mdbEnviado = "C://CYII//conteoElementos//data//enviado//RA_58270//replica.mdb"
 
+replicaRecibido = os.path.join("C://CYII//conteoElementos//data//recibido//RA_58270", "replicaREC.gdb")
+replicaEnviado = os.path.join("C://CYII//conteoElementos//data//enviado//RA_58270", "replicaENV.gdb")
+if arcpy.Exists(replicaRecibido):
+    arcpy.Delete_management(replicaRecibido)
+if arcpy.Exists(replicaEnviado):
+    arcpy.Delete_management(replicaEnviado)
+arcpy.CreateFileGDB_management("C://CYII//conteoElementos//data//recibido//RA_58270", "replicaREC")
+arcpy.CreateFileGDB_management("C://CYII//conteoElementos//data//enviado//RA_58270", "replicaENV")
 
-
-diccionario = {}
-listaDS = arcpy.ListDatasets(gdb)
+arcpy.env.workspace = mdbRecibido
+dicRecibido = {}
+listaDS = arcpy.ListDatasets()
 for ds in listaDS:
-    listaFC = arcpy.ListFeatureClasses(listaDS)
+    arcpy.env.workspace = os.path.join(mdbRecibido, ds)
+    listaFC = arcpy.ListFeatureClasses()
     for fc in listaFC:
-        diccionario = {
-            arcpy.Describe(fc).name + ": " + arcpy.GetCount(fc)
-        }
-        f.writelines(lineaFichero)
+        arcpy.CopyFeatures_management(fc, os.path.join("C://CYII//conteoElementos//data//recibido//RA_58270//replicaREC.gdb", fc))
+        arcpy.MakeTableView_management(os.path.join("C://CYII//conteoElementos//data//recibido//RA_58270//replicaREC.gdb", fc), fc + "ViewREC")
+        key = arcpy.Describe(fc).name
+        value = str(arcpy.GetCount_management(fc + "ViewREC"))
+        dicRecibido[key] = value
+        fRecibido.writelines(dicRecibido)
+fRecibido.close()
+
+arcpy.env.workspace = mdbEnviado
+dicEnviado = {}
+listaDS = arcpy.ListDatasets()
+for ds in listaDS:
+    arcpy.env.workspace = os.path.join(mdbEnviado, ds)
+    listaFC = arcpy.ListFeatureClasses()
+    for fc in listaFC:
+        arcpy.CopyFeatures_management(fc, os.path.join(
+            "C://CYII//conteoElementos//data//enviado//RA_58270//replicaENV.gdb", fc))
+        arcpy.MakeTableView_management(
+            os.path.join("C://CYII//conteoElementos//data//enviado//RA_58270//replicaENV.gdb", fc), fc + "ViewENV")
+        key = arcpy.Describe(fc).name
+        value = str(arcpy.GetCount_management(fc + "ViewENV"))
+        dicEnviado[key] = value
+        fEnviado.writelines(dicRecibido)
+fEnviado.close()
+
+
+print "Fin " + str(datetime.now().time())
